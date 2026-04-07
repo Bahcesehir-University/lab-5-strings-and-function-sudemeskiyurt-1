815e3272682955edccc4fb8662edde789b884d7c
sudemeskiyurt <sudem.eskiyurt@bahcesehir.edu.tr>
2026-04-07 15:12:46 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -158,7 +158,7 @@ MyString MyString::trim() const {
     // Hint: Use find_first_not_of and find_last_not_of
     size_t start = data.find_first_not_of(" \t\n\r");
     if (start == string ::npos) return MyString("");
-    size_t end = data.find_first_not_of(" \t\n\r");
+    size_t end = data.find_last_not_of(" \t\n\r");
     return MyString(data.substr(start, end-start+1));
     
 }
